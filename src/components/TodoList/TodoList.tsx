f0a4cb6775b5dd08f145dbf3d139d09814a76ee2
Olly Pethick <ollypethick@gmail.com>
2025-03-26 00:13:37 +0000

    Update styling

--- src/components/TodoList/TodoList.tsx
+++ src/components/TodoList/TodoList.tsx
@@ -15,27 +15,33 @@ export const TodoList = () => {
   return (
     <div aria-live="polite">
       {todos.map(({ id, completed, description }) => (
-        <div key={id}>
+        <div key={id} className="flex items-center justify-between p-2">
           <input
             id={`${id}-checkbox`}
             type="checkbox"
             onChange={() => toggleTodo(id)}
             checked={completed}
             aria-checked={completed}
+            className="m-2"
           />
-          <label htmlFor={`${id}-checkbox`}>
+          <label htmlFor={`${id}-checkbox`} className="m-2">
             <p className={completed ? 'line-through' : ''}>{description}</p>
           </label>
-          <button onClick={() => deleteTodo(id)} aria-label="Delete this todo">
+          <button
+            onClick={() => deleteTodo(id)}
+            aria-label="Delete this todo"
+            className="m-2"
+          >
             delete
           </button>
         </div>
       ))}
-      <div>
+      <div className="flex mt-3">
         <input
           value={description}
           onChange={(event) => setDescription(event.target.value)}
           onKeyUp={(event) => event.key === 'Enter' && handleAddTodo()}
+          className="mr-2 p-2"
         />
         <button
           disabled={!description.trim()}
