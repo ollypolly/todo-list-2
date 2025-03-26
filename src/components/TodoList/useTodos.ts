a9fc4ae6b59ea122f0930d8998f50e0e0242206a
Olly Pethick <ollypethick@gmail.com>
2025-03-26 00:46:09 +0000

    Add reorder function

--- src/components/TodoList/useTodos.ts
+++ src/components/TodoList/useTodos.ts
@@ -57,11 +57,24 @@ export const useTodos = () => {
   const deleteTodo = (id: string) =>
     setTodos(todos.filter((todo) => todo.id !== id));
 
+  const reorderTodo = (id: string, endIndex: number) => {
+    const newTodos = [...todos];
+
+    const startIndex = newTodos.findIndex((todo) => todo.id === id);
+    if (startIndex === -1) return;
+
+    const [movedTodo] = newTodos.splice(startIndex, 1);
+    newTodos.splice(endIndex, 0, movedTodo);
+
+    setTodos(newTodos);
+  };
+
   return {
     todos,
     addTodo,
     updateTodo,
     toggleTodo,
     deleteTodo,
+    reorderTodo,
   };
 };
